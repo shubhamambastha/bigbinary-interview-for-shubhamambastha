df5896ef05b975cfd325f30d5adad3f53d0297a2
Shubham Ambastha <shubhams.saurav@gmail.com>
2021-03-07 03:41:33 +0530

    color configured in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+const { colors } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: false,
   variants: {},
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        primary: "#1F2937",
+        secondary: "#4B5563",
+      },
+      backgroundColor: {
+        primary: "#F4F5F7",
+        secondary: "#E4E4E7",
+      },
+      borderColor: {
+        primary: "#E4E4E7",
+      },
+    },
     plugins: [],
   },
 };
