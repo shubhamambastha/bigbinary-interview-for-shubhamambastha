c81b82a11417fd7d959aebd223f4fd789c3fc9e9
Shubham Ambastha <shubhams.saurav@gmail.com>
2021-03-06 20:50:44 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import Test from "../src/components/Test";
+
+const Index = () => (
+  <div className="container bg-primary page">
+    <Test />
+  </div>
+);
+
+export default Index;
