40a49c82b55d25fbb4e689b2d5505bb8fbb41f74
Shubham Ambastha <shubhams.saurav@gmail.com>
2021-03-07 16:51:23 +0530

    creating home page component

---
+++ src/components/internal/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import TableWrapper from "../TableWrapper";
+
+const Home = (props) => {
+  return (
+    <div className="w-full">
+      <TableWrapper />
+    </div>
+  );
+};
+
+export default Home;
