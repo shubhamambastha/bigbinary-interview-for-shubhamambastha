6d8ab12ecaa390c71bfe483a8af01392f79db6c2
Shubham Ambastha <shubhams.saurav@gmail.com>
2021-03-07 17:35:38 +0530

    created status tag component

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,16 @@ module.exports = {
       textColor: {
         primary: "#1F2937",
         secondary: "#4B5563",
+        warning: "#981B1C",
+        success: "#03543F",
+        pending: "#92400F",
       },
       backgroundColor: {
         primary: "#F4F5F7",
         secondary: "#E4E4E7",
+        warning: "#FDE2E1",
+        success: "#DEF7EC",
+        pending: "#FEF3C7",
       },
       borderColor: {
         primary: "#E4E4E7",
