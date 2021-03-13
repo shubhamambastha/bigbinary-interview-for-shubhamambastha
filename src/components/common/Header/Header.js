9b04a3a37e8f35c3d5cbfd377b6f0dcdc379c58a
Shubham Ambastha <shubhams.saurav@gmail.com>
2021-03-13 23:56:01 +0530

    shadow added

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -3,7 +3,7 @@ import { CompanyLogo } from "../../../_helpers/Icons";
 
 const Header = () => {
   return (
-    <div className="h-full">
+    <div className="h-full shadow">
       <div className="w-full p-5 flex justify-center items-center bg-white z-20">
         <CompanyLogo className="w-64 h-8" />
       </div>
